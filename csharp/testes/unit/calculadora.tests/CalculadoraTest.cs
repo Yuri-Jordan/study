40126f78a18144522fc457357903155233a1cddd
Yuri Jordan <yuri.terceirizado@tce.rn.gov.br>
2025-08-10 13:14:57 -0300

    feat(csharp): each fact test init a new instance of class being tested

--- csharp/testes/unit/calculadora.tests/CalculadoraTest.cs
+++ csharp/testes/unit/calculadora.tests/CalculadoraTest.cs
@@ -1,11 +1,27 @@
 ﻿namespace calculadora.tests;
+
 using calculadora;
 
 public class CalculadoraTest
 {
+    private readonly Calculadora _sut = new(); // System Under Test
+    private readonly Guid _guid = Guid.NewGuid();
+
     [Fact]
     public void Add_DeveSomar_DoisNumerosInteiros()
     {
         Assert.Equal(3, new Calculadora().Add(1, 2));
+    }
+
+    [Fact]
+    public void Guid_MustBeDifferente_ForEachFact1()
+    {
+        Console.WriteLine($"Guid 1: {_guid}");
+    }
+
+    [Fact]
+    public void Guid_MustBeDifferente_ForEachFact2()
+    {
+        Console.WriteLine($"Guid 2: {_guid}");
     }
 }
