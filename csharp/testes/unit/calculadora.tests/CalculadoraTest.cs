e03a13683a4c6ce5be4767f862b4f3fda84c93a3
Yuri Jordan <yuri.terceirizado@tce.rn.gov.br>
2025-08-10 13:03:22 -0300

    dotnet unit tests init

---
+++ csharp/testes/unit/calculadora.tests/CalculadoraTest.cs
@@ -0,0 +1,11 @@
+﻿namespace calculadora.tests;
+using calculadora;
+
+public class CalculadoraTest
+{
+    [Fact]
+    public void Add_DeveSomar_DoisNumerosInteiros()
+    {
+        Assert.Equal(3, new Calculadora().Add(1, 2));
+    }
+}
